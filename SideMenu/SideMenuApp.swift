6d5a96b15e2be4f95735642fff6f30a6abc8908c
Vinicius Nadin <vininadin@gmail.com>
2024-02-09 15:43:58 -0300

    Initial Commit

---
+++ SideMenu/SideMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SideMenuApp.swift
+//  SideMenu
+//
+//  Created by Vinicius Nadin on 09/02/24.
+//
+
+import SwiftUI
+
+@main
+struct SideMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
